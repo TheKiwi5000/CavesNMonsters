25b0031dd16925e79edcac703edb8de4cefb5fe8
TheKiwi5000 <nzkiwi.5000@gmail.com>
2013-12-04 11:54:54 +0100

    Added Logger, Item, Player, Invntrory, Enhanced gui

--- src/pl/website/bcsn/cnm/Main.java
+++ src/pl/website/bcsn/cnm/Main.java
@@ -1,9 +1,19 @@
 package pl.website.bcsn.cnm;
 
+import pl.website.bcsn.cnm.gui.GameWindow;
+
 public class Main {
 	
+	public static String version = "0.0.1";
+	private static GameWindow gameWindow = new GameWindow("CavesNMonsters v"+version);
 	
-	
-	
+	public static void main(String[] args){
+		Logger.fine("Starting");
+		
+		
+		Logger.fine("Displaying window");
+		gameWindow.setVisible(true);
+		Logger.fine("Done");
+	}
 	
 }
